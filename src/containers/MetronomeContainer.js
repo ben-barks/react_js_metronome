a8a8ec3e959034da30a5a31464ba949674ed73ff
Barker <benbarks.v1@gmail.com>
2019-05-29 12:43:42 +0100

    base grounds set

---
+++ src/containers/MetronomeContainer.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import RangeSlider from '../components/RangeSlider';
+
+class MetronomeContainer extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      bpm: 100
+    }
+    this.handleBPMchange = this.handleBPMchange.bind(this);
+    
+  }
+
+  changeBPM(newValue){
+    this.state.bpm = newValue;
+  }
+
+  handleBPMchange(newValue){
+    const nIntervId = setInterval(changeBPM(newValue), 6);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Metronome</h2>
+        <h5>{this.state.bpm}</h5>
+        <RangeSlider handleBPMchange={this.handleBPMchange}/>
+      </div>
+    )
+  }
+}
+
+export default MetronomeContainer;
